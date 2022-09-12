e8f91fb5dcbad2271a32ab1f3417db6df924cfa6
luckystar <stevezlh@hotmail.com>
2022-09-12 20:40:52 +0800

    init: quick sort

---
+++ src/main/java/com/sort/QuickSort.java
@@ -0,0 +1,24 @@
+/**
+ * @(#) QuickSort.java 1.0 2022-09-12
+ * Copyright (c) 2022, AllNightBlues. ALL right reserved.
+ * AllNightBlues PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.sort;
+
+/**
+ * @ClassName QuickSort
+ * @description:
+ * @AUTHOR AllNightBlues
+ * @Date 2022/09/12 20:38
+ * @Version 1.0
+ **/
+public class QuickSort {
+
+    public int[] sortArray(int[] nums) {
+        return sort(nums, 0, nums.length - 1);
+    }
+
+    public static int[] sort(int[] array, int start, int end) {
+        return array;
+    }
+}
